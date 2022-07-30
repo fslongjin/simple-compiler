a912c7a98dd58e202547e22fbd4b4cc6697b400b
fslongjin <longjin@RinGoTek.cn>
2022-07-30 16:49:45 +0800

    创建了parser

---
+++ parser/parser.cpp
@@ -0,0 +1,9 @@
+//
+// Created by longjin on 22-7-29.
+//
+
+#include "parser.h"
+
+namespace parser
+{
+} // parser
---
+++ parser/parser.h
@@ -0,0 +1,18 @@
+//
+// Created by longjin on 22-7-29.
+//
+
+#ifndef _PARSER_H_
+#define _PARSER_H_
+
+namespace parser
+{
+
+	class parser
+	{
+
+	};
+
+} // parser
+
+#endif //_PARSER_H_
